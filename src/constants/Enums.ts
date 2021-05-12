0b1f5776ab98d790dcb05aee51a7639aeb89afeb
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2021-05-12 13:48:24 +0530

    feat: all the endpoints

--- src/constants/Enums.ts
+++ src/constants/Enums.ts
@@ -12,3 +12,31 @@ export enum FetchTypes {
 	GET = 'GET',
 	POST = 'POST'
 }
+
+export enum GeneratorEndpoints {
+	BATSLAP = 'batslap',
+	BEAUTIFUL = 'beautiful',
+	BOBROSS = 'bobross',
+	EVIL = 'evil',
+	FACEPALM = 'facepalm',
+	GARBAGE = 'garbage',
+	LIO = 'lio',
+	MISSING = 'missing',
+	RELIGION = 'religion',
+	RIP = 'rip',
+	TINDER = 'tinder',
+	TRIGGERED = 'triggered',
+	WANTED = 'wanted'
+}
+
+export enum OverlayEndpoints {
+	RAINBOW = 'rainbow',
+	APPROVED = 'sign/approved',
+	REJECTED = 'sign/rejected'
+}
+
+export enum TextEndpoints {
+	ACHIEVEMENT = 'achievement',
+	CMM = 'cmm',
+	ILLEGAL = 'illegal'
+}
--- src/lib/FlykraClient.ts
+++ src/lib/FlykraClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { FetchTypes, FlykraBase, FlykraHeaders } from '../constants/Enums';
+import { FetchTypes, FlykraBase, FlykraHeaders, GeneratorEndpoints, OverlayEndpoints, TextEndpoints } from '../constants/Enums';
 import fetch, { Response } from 'node-fetch';
 
 export default class FlykraClient {
@@ -7,42 +7,56 @@ export default class FlykraClient {
 	public secret: string;
 	public basePath: string;
 
+	private headers = {
+		'Content-Type': FlykraHeaders.ContentTypeJSON,
+		'User-Agent': FlykraHeaders.UserAgent,
+		'Accept-Encoding': FlykraHeaders.AcceptEncoding
+	};
+
 	public constructor(options: FlykraClientOptions) {
+		if (!options.secret) throw new Error('An API secret token must be provided to initialize Flykra client.');
+
 		this.secret = options.secret;
 		this.basePath = options.basePath ? options.basePath : FlykraBase.APIURL;
 	}
 
-	private get<T>(endpoint: string): Promise<T> {
+	// Generators
+	public batSlap = (avatar: string, target: string) => this.post(GeneratorEndpoints.BATSLAP, { avatar, target }) as BaseAPIImageResponse;
+	public beautiful = (avatar: string) => this.post(GeneratorEndpoints.BEAUTIFUL, { avatar }) as BaseAPIImageResponse;
+	public bobRoss = (avatar: string) => this.post(GeneratorEndpoints.BOBROSS, { avatar }) as BaseAPIImageResponse;
+	public evil = (avatar: string) => this.post(GeneratorEndpoints.EVIL, { avatar }) as BaseAPIImageResponse;
+	public facepalm = (avatar: string) => this.post(GeneratorEndpoints.FACEPALM, { avatar }) as BaseAPIImageResponse;
+	public garbage = (avatar: string) => this.post(GeneratorEndpoints.GARBAGE, { avatar }) as BaseAPIImageResponse;
+	public lio = (avatar: string) => this.post(GeneratorEndpoints.LIO, { avatar }) as BaseAPIImageResponse;
+	public missing = (avatar: string, text: string) => this.post(GeneratorEndpoints.MISSING, { avatar, text }) as BaseAPIImageResponse;
+	public religion = (avatar: string) => this.post(GeneratorEndpoints.RELIGION, { avatar }) as BaseAPIImageResponse;
+	public rip = (avatar: string) => this.post(GeneratorEndpoints.RIP, { avatar }) as BaseAPIImageResponse;
+	public tinder = (avatar: string, target: string) => this.post(GeneratorEndpoints.TINDER, { avatar, target }) as BaseAPIImageResponse;
+	public triggered = (avatar: string) => this.post(GeneratorEndpoints.TRIGGERED, { avatar }) as BaseAPIImageResponse;
+	public wanted = (avatar: string) => this.post(GeneratorEndpoints.WANTED, { avatar }) as BaseAPIImageResponse;
 
-		const url = `${this.basePath}/${endpoint}`;
+	// Overlays
+	public rainbow = (avatar: string) => this.post(OverlayEndpoints.RAINBOW, { avatar }) as BaseAPIImageResponse;
+	public approved = (avatar: string) => this.post(OverlayEndpoints.APPROVED, { avatar }) as BaseAPIImageResponse;
+	public rejected = (avatar: string) => this.post(OverlayEndpoints.REJECTED, { avatar }) as BaseAPIImageResponse;
 
-		const headers = {
-			'authorization': this.secret,
-			'Content-Type': FlykraHeaders.ContentTypeJSON,
-			'User-Agent': FlykraHeaders.UserAgent,
-			'Accept-Encoding': FlykraHeaders.AcceptEncoding
-		};
+	// Text
+	public achievement = (avatar: string, text: string) => this.post(TextEndpoints.ACHIEVEMENT, { avatar, text }) as BaseAPIImageResponse;
+	public changeMyMind = (avatar: string, text: string) => this.post(TextEndpoints.CMM, { avatar, text }) as BaseAPIImageResponse;
+	public illegal = (avatar: string, text: string) => this.post(TextEndpoints.ILLEGAL, { avatar, text }) as BaseAPIImageResponse;
 
-		return fetch(url, { method: FetchTypes.GET, headers: { ...headers } }).then((r: Response) => {
-			if (r.ok) return r.json();
-			throw new Error(r.statusText);
+	// Base Requests
+	public get<T>(endpoint: string): Promise<T> {
+		return fetch(`${this.basePath}/${endpoint}`, { method: FetchTypes.GET, headers: { ...this.headers, authorization: this.secret } }).then((r: Response) => {
+			if (r.ok) return r.buffer();
+			return r.json();
 		});
 	}
 
 	private post<T>(endpoint: string, data = {}): Promise<T> {
-
-		const url = `${this.basePath}/${endpoint}`;
-
-		const headers = {
-			'authorization': this.secret,
-			'Content-Type': FlykraHeaders.ContentTypeJSON,
-			'User-Agent': FlykraHeaders.UserAgent,
-			'Accept-Encoding': FlykraHeaders.AcceptEncoding
-		};
-
-		return fetch(url, { method: FetchTypes.POST, headers: { ...headers }, body: JSON.stringify(data) }).then((r: Response) => {
-			if (r.ok) return r.json();
-			throw new Error(r.statusText);
+		return fetch(`${this.basePath}/${endpoint}`, { method: FetchTypes.POST, headers: { ...this.headers, authorization: this.secret }, body: JSON.stringify(data) }).then((r: Response) => {
+			if (r.ok) return r.buffer();
+			return r.json();
 		});
 	}
 
@@ -51,4 +65,11 @@ export default class FlykraClient {
 interface FlykraClientOptions {
 	secret: string;
 	basePath?: string;
+}
+
+type BaseAPIImageResponse = Promise<Buffer[] | APIResponse>;
+
+interface APIResponse {
+	success: boolean;
+	body: string;
 }
